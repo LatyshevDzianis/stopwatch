0272f78754c0d8ebbf637a69baab2c7584dba6e3
Dzianis Latyshev <latyshevdzianis@gmail.com>
2020-07-17 14:44:30 +0300

    Add tabs navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import StopWatch from './components/StopWatch/StopWatch';
 import './App.css'
 import Timer from './components/Timer/Timer';
+import openTab from './script'
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <div className="tab">
+        <button className="tablinks" onClick={event => openTab(event, 'Stopwatch')}>Секундомер</button>
+        <button className="tablinks" onClick={event => openTab(event, 'Timer')}>Таймер</button>
+      </div>
+
+      <div id="Stopwatch" className="tabcontent">
+        <StopWatch />
+      </div>
+
+      <div id="Timer" className="tabcontent">
+        <Timer />
+      </div>
     </div>
   );
 }
